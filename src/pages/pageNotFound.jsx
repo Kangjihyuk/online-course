73a0f2b3434c10c70c9086679328baebe0481b2b
khusni mubarok <husnirqw@gmail.com>
2025-02-11 10:26:38 +0700

    next

--- src/pages/pageNotFound.jsx
+++ src/pages/pageNotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "../components/Elements/image";
 
 const pageNotFound = () => {
-  return <Image src="../../images/assets/404-pageNotFound.png" alt={"404"} />;
+  return <Image src={"../../images/assets/404-pageNotFound.png"} alt={"404"} />;
 };
 
 export default pageNotFound;
